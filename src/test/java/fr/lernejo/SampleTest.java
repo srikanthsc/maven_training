24805dbf3c4a0f648d13720be61c4222ff76a7d3
srikanth collaty <srikanthcollaty92@gmail.com>
2021-11-30 14:16:44 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,16 @@
+package fr.lernejo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void op() {
+    }
+
+    @Test
+    void fact() {
+    }
+}
